a20de9052007d7d57d25bb3718fb12c3b0952842
Lồ Sìn Dậu <luoshanyou75588@gmail.com>
2022-09-22 20:47:07 +0700

    config againnnn

--- modules/config.php
+++ modules/config.php
@@ -1,11 +1,19 @@
 <?php
-define("HOST","localhost");
-define("DB","db_furniture");
-define("USER","root");
-define("PASSWORD","");
+define("HOST", "localhost");
+define("DB", "db_furniture");
+define("USER", "root");
+define("PASSWORD", "");
 
-define('APPROOT',dirname(dirname(__FILE__)));
-define('URLROOT',dirname($_SERVER['PHP_SELF']));
-define('IMAGE', URLROOT.'/public/img');
-define('JSFILE', URLROOT.'/public/js');
-define('CSSFILE', URLROOT.'/public/css');
+//Lấy hosing hiện tại
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+    $path = "https://";
+} else {
+    $path = "http://";
+}
+$path .= $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']);
+
+define('APPROOT', dirname(dirname(__FILE__)));
+define('URLROOT', $path);
+define('IMAGE', URLROOT . '/public/img');
+define('JSFILE', URLROOT . '/public/js');
+define('CSSFILE', URLROOT . '/public/css');
