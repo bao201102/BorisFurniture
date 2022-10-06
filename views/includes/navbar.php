c53227bdcdd6a5ab4ced6f28415ced883ee8aac0
Nguyễn Ngọc Bảo <bao201102@gmail.com>
2022-10-07 00:05:05 +0700

    cảt

--- views/includes/navbar.php
+++ views/includes/navbar.php
@@ -35,40 +35,55 @@
                         </a>
 
                         <!-- shopping cart -->
-                        <form class="header-cart-list shadow-lg">
-                            <ul class="cart-product-list mb-2">
-                                <li class="cart-product-item d-flex mb-2">
-                                    <img src="./img/Classic-Lamp.jpg" alt="" style="width: 20%;">
+                        <div class="header-cart-list shadow-lg">
+                            <?php if (isset($_SESSION['cart'])) : ?>
+                                <ul class="cart-product-list mb-2">
+                                    <?php foreach ($_SESSION['cart'] as $prod) : extract($prod) ?>
 
-                                    <div class="cart-product-description">
-                                        <div class="cart-product-main-des mb-2 fs-4">
-                                            Classic Lamp
-                                        </div>
-                                        <div class="cart-product-sub-des" style="color: rgb(155, 150, 150);">
-                                            <span class="cart-product-quantity">1</span>
-                                            <span>x</span>
-                                            <span>450,000
-                                                <span class="pSymbol">$</span>
-                                            </span>
-                                        </div>
+                                        <li class="cart-product-item d-flex mb-2">
+                                            <img src="<?= IMAGE ?>/<?= $prod_img ?>" alt="" style="width: 20%;">
+
+                                            <div class="cart-product-description">
+                                                <div class="cart-product-main-des mb-2 fs-4">
+                                                    <?= $prod_name ?>
+                                                </div>
+                                                <div class="cart-product-sub-des" style="color: rgb(155, 150, 150);">
+                                                    <span class="cart-product-quantity"><?= $prod_quantity ?></span>
+                                                    <span>x</span>
+                                                    <span>$<?= $prod_price ?>.00</span>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <form action="<?= URLROOT ?>/Cart/deleteProduct/<?= $prod_id ?>" method="POST">
+                                                    <button style="border: none; background: white;">
+                                                        <span class="material-symbols-outlined cart-delete">
+                                                            close
+                                                        </span>
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </li>
+
+                                    <?php endforeach; ?>
+                                </ul>
+                                <div class="cart-subtotal d-flex fs-4 justify-content-center align-items-center border-top border-bottom py-3 fs-5">
+                                    <label class="mx-1">Subtotal:</label>
+                                    <div class="fs-5 fw-semibold">1,000,000<span class="pSymbol">$</span>
                                     </div>
-
-                                    <span class="material-symbols-outlined cart-delete">
-                                        close
-                                    </span>
-                                </li>
-
-                            </ul>
-                            <div class="cart-subtotal d-flex fs-4 justify-content-center align-items-center border-top border-bottom py-3 fs-5">
-                                <label class="mx-1">Subtotal:</label>
-                                <div class="fs-5 fw-semibold">1,000,000<span class="pSymbol">$</span>
                                 </div>
-                            </div>
-                            <div class="cart-button d-flex flex-column justify-content-center mt-3">
-                                <a class="btn btn-primary" href="<?= URLROOT ?>/Home/cart" type="submit">View Cart</a>
-                                <a class="btn btn-outline-primary mt-2" href="<?= URLROOT ?>/Home/checkout" type="submit">Checkout</a>
-                            </div>
-                        </form>
+                                <div class="cart-button d-flex flex-column justify-content-center mt-3">
+                                    <a class="btn btn-primary" href="<?= URLROOT ?>/Home/cart" type="submit">View Cart</a>
+                                    <a class="btn btn-outline-primary mt-2" href="<?= URLROOT ?>/Home/checkout" type="submit">Checkout</a>
+                                </div>
+
+                            <?php else : ?>
+
+                                <div class="text-center">
+                                    <p>You have no products in your cart</p>
+                                </div>
+
+                            <?php endif; ?>
+                        </div>
                     </li>
 
                     <!-- login -->
