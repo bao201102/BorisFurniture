d3b2ee2f02a3e3699cab568de5920d90dc425302
Nguyễn Ngọc Bảo <bao201102@gmail.com>
2022-10-26 17:03:33 +0700

    a

--- models/CustomerModel.php
+++ models/CustomerModel.php
@@ -1,9 +1,59 @@
 <?php
 class CustomerModel
 {
-    public function __contruct()
+    var $cus_id;
+    var $user_id;
+    var $firstname;
+    var $lastname;
+    var $birthday;
+    var $phone;
+    var $status;
+
+    public function getCusId()
     {
-        
+        return $this->cus_id;
+    }
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+    public function getBirthday()
+    {
+        return $this->phone;
+    }
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+
+    public function __contruct($cus_id, $user_id, $firstname, $lastname, $birthday, $phone, $status)
+    {
+        $this->cus_id = $cus_id;
+        $this->user_id = $user_id;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->birthday = $birthday;
+        $this->phone = $phone;
+        $this->status = $status;
+    }
+
+    public function getCustomerList()
+    {
+        $link = null;
+        taoKetNoi($link);
+        $result = chayTruyVanTraVeDL($link, "select * from tbl_customer where status = 1");
+        $data = $result;
+        giaiPhongBoNho($link, $result);
+        return $data;
     }
 
     public function getCustomerByUserId($user_id)
